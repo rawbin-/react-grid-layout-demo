ddc6e9d07c2c0f53e8c2b2a02b10fc607152d484
rawbin <fcrshjx44@gmail.com>
2025-07-24 00:50:25 +0800

    feat: 元素尺寸会被动态接管

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import RGL, { WidthProvider } from 'react-grid-layout'
+import './App.css'
 
 const ReactGridLayout = WidthProvider(RGL)
 function App() {
 
-  return (
-    <>
-        <ReactGridLayout
-        >
-            <div key="a">a</div>
-            <div key="b">b</div>
-            <div key="c">c</div>
+    return <div className="wrapper">
+        <ReactGridLayout>
+            <div className="item item-a" key="a">a</div>
+            <div className="item item-b" key="b">b</div>
+            <div className="item item-c" key="c">c</div>
+            <div className="item item-d" key="d">d</div>
         </ReactGridLayout>
-    </>
-  )
+    </div>
 }
 
 export default App
