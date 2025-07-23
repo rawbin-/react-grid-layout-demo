dc6c6781e6c51bcc20c739fdaae984863e7d97e6
rawbin <fcrshjx44@gmail.com>
2025-07-23 23:56:07 +0800

    feat: 最简单的reac-grid-layout Demo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import RGL from 'react-grid-layout'
+function App() {
+
+  return (
+    <>
+        <RGL
+        >
+            <div key="a">a</div>
+            <div key="b">b</div>
+            <div key="c">c</div>
+        </RGL>
+    </>
+  )
+}
+
+export default App
